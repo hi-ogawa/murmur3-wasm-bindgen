e1193bd8b4016a47eb1bd07099e603efbf913c53
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-10-07 10:19:01 +0900

    chore: update deps + style

--- src-js/bundle.ts
+++ src-js/bundle.ts
@@ -2,8 +2,8 @@ import pkgInit from "../pkg/index";
 import pkgWasm from "../pkg/index_bg.wasm"; // let tsup/esbuild bundle wasm binary ~ 20KB
 
 export async function init() {
-  const wasmModule = await WebAssembly.compile(pkgWasm as any as Uint8Array);
-  await pkgInit(wasmModule);
+	const wasmModule = await WebAssembly.compile(pkgWasm as any as Uint8Array);
+	await pkgInit(wasmModule);
 }
 
 // re-export
--- src-js/fuzz.test.ts
+++ src-js/fuzz.test.ts
@@ -4,22 +4,22 @@ import { beforeAll, describe, expect, it } from "vitest";
 import * as bundle from "../dist/bundle";
 
 beforeAll(async () => {
-  await bundle.init();
+	await bundle.init();
 });
 
 describe("fuzz", () => {
-  it("murmur3_32", () => {
-    fc.assert(
-      fc.property(
-        fc.uint8Array(),
-        fc.integer({ min: 0, max: 2 ** 32 - 1 }),
-        (input, seed) => {
-          const l = bundle.murmur3_32(input, seed);
-          const r = murmur3_32(input, seed);
-          expect(l).toBe(r);
-        }
-      ),
-      { verbose: true, numRuns: 10 ** 4 }
-    );
-  });
+	it("murmur3_32", () => {
+		fc.assert(
+			fc.property(
+				fc.uint8Array(),
+				fc.integer({ min: 0, max: 2 ** 32 - 1 }),
+				(input, seed) => {
+					const l = bundle.murmur3_32(input, seed);
+					const r = murmur3_32(input, seed);
+					expect(l).toBe(r);
+				},
+			),
+			{ verbose: true, numRuns: 10 ** 4 },
+		);
+	});
 });
